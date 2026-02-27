721e4fe90d9f395fcd7ac38ba82fc88554ddd479
!Drugdosing24! <mpierce1026@aol.com>
2026-02-27 10:40:31 -0500

    Update README for public data repository

--- download_model.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import snapshot_download
-
-# Download the Dolphin3.0-Llama3.1-8B model to D: drive
-repo_id = "dphn/Dolphin3.0-Llama3.1-8B"
-local_dir = "D:/Dolphin3.0-Llama3.1-8B"
-
-print(f"Starting download of {repo_id} to {local_dir}...")
-snapshot_download(repo_id=repo_id, local_dir=local_dir)
-print("Download completed!")
